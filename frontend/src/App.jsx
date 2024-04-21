c4f0119c93f6f083785dd2c678293fc31592267a
Diego Hernandez <diegofootball19@gmail.com>
2024-04-21 16:49:03 -0500

    testing react api with reacdt frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,13 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState("")
+  const [count, setCount] = useState(null)
   useEffect (() => {
   
       async function fetchData() {
         const response = await fetch("http://localhost:1234/");
         const data = await response.json()
-        console.log(data)
-        setCount(JSON.stringify(data))
+        setCount(data.places[0].formattedAddress)
       }
 
       fetchData() 
@@ -23,7 +22,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        {String(count)}
+        {count}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
