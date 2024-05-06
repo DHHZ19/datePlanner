f30a89866f4e799a24c8ab087affd2302fb4ca18
Diego Hernandez <diegofootball19@gmail.com>
2024-05-06 18:13:34 -0500

    remove uneeded styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -56,8 +56,9 @@ function App() {
         )} */}
 
         <label>
-          Search albums:
+          Search Resturants:
           <input value={query} onChange={(e) => setQuery(e.target.value)} />
+          <br />
           {query}
         </label>
 
