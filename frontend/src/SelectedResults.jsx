0776a858e443e2cba71ad0462defc503b28f0bdc
Diego Hernandez <diegofootball19@gmail.com>
2024-05-19 14:05:49 -0500

    update selected resturants

--- frontend/src/SelectedResults.jsx
+++ frontend/src/SelectedResults.jsx
@@ -1,7 +1,7 @@
 export default function SelectedResults({ selectedResults }) {
   return (
     <div className="flex flex-col text-center px-2">
-      <h3 className="font-bold font-sans text-2xl">Selected Resturants: </h3>
+      <h3 className="font-bold font-sans text-xl">Selected Resturants: </h3>
       {selectedResults.map((child) => (
         <div key={child.PlaceID} className="flex items-center flex-col">
           <span className="font-bold text-xl">{child.Name}</span>
